455ea57ad656dc834f57b387ed2cfadde0906a4a
Shoichi Kaji <skaji@cpan.org>
2018-02-17 16:56:01 +0900

    hoge

---
+++ test.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use HTTP::Tinyish;
+
+for my $klass ("HTTP::Tiny", "HTTP::Tinyish", "IO::Socket::SSL", "Net::SSLeay", "LWP", "LWP::Protocol::https") {
+    my $version = eval "require $klass; $klass->VERSION" || "N/A";
+    print "$klass $version\n";
+}
+print "\n";
+
+my $fail = { status => '000', reason => '?', content => '' };
+for my $backend (map "HTTP::Tinyish::$_", qw( LWP HTTPTiny Curl Wget )) {
+    local $HTTP::Tinyish::PreferredBackend = $backend;
+    my $http = HTTP::Tinyish->new;
+    my $res = eval { $http->get("https://metacpan.org/") } || $fail;
+    print "---> $backend\n";
+    print "$res->{status} $res->{reason}\n";
+    print $res->{content} unless $res->{success};
+}
